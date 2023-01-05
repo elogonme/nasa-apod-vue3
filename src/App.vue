6c23a6fb883588cbb1bf044841a7134a0aa1114d
Eldar Humbatov <elogon@gmail.com>
2023-01-04 20:23:01 -0500

    add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+const switchDarkMode = () =>
+  (theme.global.name.value = theme.global.current.value.dark
+    ? "light"
+    : "dark");
 </script>
 
 <template>
-  <div class="wrapper">
+  <v-container>
     <nav>
-      <RouterLink to="/" class="nav-link">Home</RouterLink>
+      <v-icon icon="mdi-home-outline" />
+      <RouterLink to="/" class="nav-link"> Home</RouterLink>
+      <v-icon icon="mdi-information-outline" />
       <RouterLink to="/about" class="nav-link">About</RouterLink>
+      <v-btn flat text size="sm" class="float-right" @click="switchDarkMode">
+        <v-icon icon="mdi-theme-light-dark"></v-icon>
+      </v-btn>
     </nav>
-    <RouterView />
-  </div>
+    <v-row justify="center">
+      <v-col>
+        <RouterView />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
 .nav-link {
-  padding: 5px 5px;
+  padding: 5px 10px 5px 5px;
+  color: rgb(0, 189, 186);
+}
+</style>
+
+<style lang="scss">
+#app {
+  text-align: center;
+  // color: #adadad;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,34 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+// import "./assets/main.css";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(vuetify);
 app.mount("#app");
