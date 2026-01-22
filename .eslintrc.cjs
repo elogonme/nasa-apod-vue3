e2704056e19dbc7c864f421999d09c15ffba9b6d
Eldar Humbatov <elogon@gmail.com>
2026-01-22 10:23:50 -0500

    lint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
